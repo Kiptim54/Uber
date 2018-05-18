d298cf8c76cc90e46756215a8c719557e45dd5e6
Kiptim <kiptim54@gmail.com>
2018-05-18 17:52:28 +0300

    add car forms

--- uberdriver/urls.py
+++ uberdriver/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     url(r'^$', views.home_page, name='home'),
     # url(r'^signup/' , views.sign_up, name='driversignup'),
     url(r'^profile$', views.create_profile, name='createprofile'),
-    url(r'^profile/car', views.Car_profile, name='carprofile'),
+    url(r'^profile/car', views.car_profile, name='carprofile'),
 
     
 
