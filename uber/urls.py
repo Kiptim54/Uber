fba4ddc984017761f7386be7b4c46febd4a43c30
Kiptim <kiptim54@gmail.com>
2018-05-18 11:17:44 +0300

    add created a landing page for both customer and driver apps

--- uber/urls.py
+++ uber/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^customer/', include('ubercustomer.urls')),
     url(r'^driver/', include('uberdriver.urls')),
+    url(r'^home/', views.welcome_user, name="joint"),
 ]
