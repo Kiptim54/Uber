919227a7d3bc6d55ae1e0b71e315ba57973c7d45
Kiptim <kiptim54@gmail.com>
2018-05-18 12:28:15 +0300

    add models in both apps

--- ubercustomer/models.py
+++ ubercustomer/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from uberdriver.models import Location, User, Destination
 
-# Create your models here.
+class Passenger_profile(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
+    name=models.CharField(max_length=50)
+    location=models.ForeignKey(Location)
+    pickupoint=models.ForeignKey(Location)
+    passenger_destination=models.ForeignKey(Destination)
+
+    def __str__(self):
+        return self.name
--- uberdriver/models.py
+++ uberdriver/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Car(models.Model):
@@ -6,21 +7,39 @@ class Car(models.Model):
     numberplate=models.CharField()
     seats=models.IntegerField()
 
+    def __str__(self):
+        return self.Brand
+
 class Location(models.Model):
     Longitude=models.IntegerField()
     latitude=models.IntegerField()
 
+    def __int__(self):
+        return self.Longitude
+
 class Destination(models.Model):
     name=models.CharField()
 
-class Driver(models.Model):
+    def __str__(self):
+        return self.name
+
+class Driver_profile(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
     name=models.CharField(max_length=50)
     car=models.ForeignKey(Car)
     pickup=models.ForeignKey(Location)
     driver_destination=models.ForeignKey(Destination)
 
-class Passenger(models.Model):
+    def __str__(self):
+        return self.name
+
+
+class Passenger_profile(models.Model):
+    user=models.ForeignKey(User, on_delete=models.CASCADE)
     name=models.CharField(max_length=50)
     location=models.ForeignKey(Location)
     pickupoint=models.ForeignKey(Location)
     passenger_destination=models.ForeignKey(Destination)
+
+    def __str__(self):
+        return self.name
