c881955bb3a13d2e35bd09c38b86d6568be931a3
Kiptim <kiptim54@gmail.com>
2018-05-20 09:52:28 +0300

    add customer can see all available rides

--- ubercustomer/views.py
+++ ubercustomer/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from uberdriver.models import Driver_profile, Destination, Car,Location
+from .models import Passenger_profile
 
 # Create your views here.
 def home_page(request):
@@ -9,3 +11,12 @@ def home_page(request):
     '''
     title="Ride | Customer "
     return render(request, 'customer/index.html', {"title":title})
+
+def display_rides(request):
+    '''
+    function that displays all available rides to the customer
+    '''
+    title="Rides | Available rides"
+    rides=Destination.objects.all()
+    print(rides)
+    return render(request, 'customer/all_rides.html', {"title":title, "rides":rides})
